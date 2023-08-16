d0146070bcd8801c91cf61ca357abe66a28b60bb
Gu Jianing <67964916+H2SO4Hasegawa@users.noreply.github.com>
2023-08-16 04:18:50 +0100

    Update 0-create-swap.sh
    Updated Erroneous Code

--- scripts/0-create-swap.sh
+++ scripts/0-create-swap.sh
@@ -4,11 +4,11 @@ sudo chmod 600 swapfile
 sudo mkswap swapfile 
 sudo swapon swapfile
 sudo fallocate -l 4G swapfile_BIG
-sudo chmod 600 swapfile 
-sudo mkswap swapfile 
-sudo swapon swapfile
+sudo chmod 600 swapfile_BIG 
+sudo mkswap swapfile_BIG 
+sudo swapon swapfile_BIG
 sudo fallocate -l 16G swapfile_BIGGER
-sudo chmod 600 swapfile 
-sudo mkswap swapfile 
-sudo swapon swapfile
+sudo chmod 600 swapfile_BIGGER 
+sudo mkswap swapfile_BIGGER 
+sudo swapon swapfile_BIGGER
 sudo swapon --show
